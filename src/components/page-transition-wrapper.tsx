dbc0d56dd455099203928761161452571adfaf19
Daniel Kliewer <danielkliewer@gmail.com>
2025-06-22 12:58:18 -0500

    a

--- src/components/page-transition-wrapper.tsx
+++ src/components/page-transition-wrapper.tsx
@@ -1,26 +1,9 @@
 "use client";
 
-import { AnimatePresence, motion } from "framer-motion";
-import { usePathname } from "next/navigation";
 import React from "react";
 
 const PageTransitionWrapper = ({ children }: { children: React.ReactNode }) => {
-  const pathname = usePathname();
-
-  return (
-    <AnimatePresence mode="wait" initial={false}>
-      <motion.div
-        key={pathname}
-        initial={{ opacity: 0, scale: 0.98, skewX: 5 }}
-        animate={{ opacity: 1, scale: 1, skewX: 0 }}
-        exit={{ opacity: 0, scale: 1.02, skewX: -5 }}
-        transition={{ duration: 0.4, ease: "easeOut" }}
-        className="flex-grow" // Ensure it takes up available space
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
-  );
+  return <>{children}</>;
 };
 
 export default PageTransitionWrapper;
